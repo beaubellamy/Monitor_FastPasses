9cdc977df126e711261aeb126fdc271283b32e0c
beaubellamy <beau.bellamy@hotmail.com>
2019-03-02 17:19:23 +1100

    Generalised a number of hard coded values. Extended the code to select the first fast pass

--- Monitor_FastPasses/Monitor_FastPasses.py
+++ Monitor_FastPasses/Monitor_FastPasses.py
@@ -2,18 +2,25 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import selenium.common.exceptions as seleniumException
+import credentials
 
-chromdriver = "C:\\Users\\bbel1\\AppData\\Local\\Continuum\\anaconda3\\Lib\\site-packages\\chromedriver_2.41.exe"
+chromdriver = credentials.chromedriver# = "C:\\Users\Beau\Anaconda3\Lib\site-packages\chromedriver.exe"
 driver = webdriver.Chrome(chromdriver)
 
-user = "YOUR-USERNAME"
-pwd = "YOUR-PASSWORD"
-guests = ["guest-CAFEF00D-6502-0000-0000-000303002409-unselected",
-          "guest-CAFEF00D-6502-0000-0000-000303002525-unselected",
-          "guest-CAFEF00D-6502-0000-0000-000303002476-unselected"]
+user = credentials.username
+pwd = credentials.password
+guests = credentials.guests
 
-dates = ["18"]
-parks = {"17": "Epcot", "18": "Hollywood Studios", "20": "Magic Kingdom", "22": "Animal Kingdom"}
+month = "March"
+dates = ["5"]
+parks = {"4": "Epcot", "5": "Hollywood Studios", "2": "Magic Kingdom", "3": "Animal Kingdom"}
+EpcotFastPasses = []
+HollywoodFastPasses = ["Fantasmic","Star Tours","Toy Story"]
+MagicFastPasses = []
+AnimalFastPasses = []
+selectedFastPasses = {"4": {}, "5": {}, "2": {}, "3": {}}
+
+
 
 driver.get("https://disneyworld.disney.go.com/login/")
 # Enter the login details
@@ -33,54 +40,118 @@ elem.send_keys(Keys.RETURN)
 for guest in guests:
      sleep(5)
      driver.find_element_by_xpath("""//*[@id="""+'"'+guest+'"'+"""]/div""").click()
-   
-    
-driver.find_element_by_xpath("""//*[@id="selectPartyPage"]/div[3]/div/div[2]/div""").click()
-sleep(2)
-elem = driver.find_element_by_css_selector(".current-month.ng-binding")
+ 
 
-while (elem.text != "February"):
-    elem = driver.find_element_by_class_name("next-month").click()
+for date in dates:
+
+    driver.find_element_by_xpath("""//*[@id="selectPartyPage"]/div[3]/div/div[2]/div""").click()
+    sleep(2)
     elem = driver.find_element_by_css_selector(".current-month.ng-binding")
 
-
-elem = driver.find_elements_by_css_selector(".day.ng-binding.ng-scope")
-
-dayIdx = []
-for i in range(len(elem)):
-   if (elem[i].text in dates):
-       dayIdx.append(i)
-
-# loop through each dayIdx
-# for day in dayIdx:
-day = 29
-
-elem[day].click()
-
-key = elem[day].text
-
-sleep(2)
-# Find each park link
-elements = driver.find_elements_by_css_selector(".park.ng-scope")
-
-parkIdx = 0
-for i in range(len(elements)):
-    if (parks[key] in elements[i].text):
-        parkIdx = i
-        break
-
-# click on the park
-elements[parkIdx].click()
+    while (elem.text != month):
+        elem = driver.find_element_by_class_name("next-month").click()
+        elem = driver.find_element_by_css_selector(".current-month.ng-binding")
 
 
+    elem = driver.find_elements_by_css_selector(".day.ng-binding.ng-scope")
 
-# Confirm "Continue with guest" is available
-try:
-    element = driver.find_element_by_css_selector(".button.ng-binding.primary")
-    element.click()
-except seleniumException.ElementNotVisibleException as exception:
-    print ("Members already have the maximum number of fast passes for this day")
+    
+    for i in range(len(elem)):
+        if (elem[i].text == date):
+            day = i
 
-print ("got past the exception")
+    day = 16
+
+    elem[day].click()
+
+    key = elem[day].text
+
+    sleep(2)
+    # Find each park link
+    elements = driver.find_elements_by_css_selector(".park.ng-scope")
+
+    parkIdx = 0
+    for i in range(len(elements)):
+        if (parks[key] in elements[i].text):
+            parkIdx = i
+            break
+
+    # click on the park
+    elements[parkIdx].click()
+
+
+
+    # Confirm "Continue with guest" is available
+    try:
+        element = driver.find_element_by_css_selector(".button.ng-binding.primary")
+        element.click()
+    except seleniumException.ElementNotVisibleException as exception:
+        print ("Members already have the maximum number of fast passes for this day")
+    except seleniumException.NoSuchElementException as exception:
+        pass
+        # On the fast pass page
+
+
+    # Fast Pass page
+    while len(selectedFastPasses[date]) < 4:
+    
+        
+        # Select the filter by time
+        timeFilter = driver.find_element_by_css_selector(".timeFilterOptionBox.ng-scope")
+        elements = timeFilter.find_elements_by_class_name("ng-scope")
+
+        # click on the desired filter ??????
+        for elem in elements:
+            if elem.text == "Afternoon":
+                elem.click()
+                break
+
+        sleep(5)
+        # Check if the desired ride has an available fast pass.
+        experiences = driver.find_elements_by_class_name("experienceNameLand")
+        experienceTimes = driver.find_elements_by_class_name("available-times-container")
+
+        #Figure out which park we need to use the fast pass list from
+        # HollywoodFastPasses
+
+        for idx, experience in enumerate(experiences):
+            ride = experience.text.split("\n")[0]
+
+            # Check if the ride is in the fast pass list.
+            if any(substring in ride for substring in HollywoodFastPasses):
+                availableTimes = experienceTimes[idx].find_elements_by_css_selector(".availableTime.ng-scope")
+                
+                # Remove the ride from the fast pass list, so we dont find it the next time round.
+                idx = [i for i, s in enumerate(HollywoodFastPasses) if s in ride]
+                del HollywoodFastPasses[idx[0]]
+
+                if availableTimes:
+                    if not selectedFastPasses[date]:
+                        selectedFastPasses[date] = {ride: availableTimes[0].text} # Add the first time value
+                        # select the time to add to the fast pass list
+                        availableTimes[0].click()
+
+                    else:
+                        # check each time has a minimum gap between existing fast pass times
+                        print ("Add additional fast passes")
+                        continue
+
+                    # Confirm selection
+                    driver.find_element_by_css_selector(".ng-scope.button.confirm.tertiary").click()
+                    # Contnue with another Fast pass on the same day
+                    driver.find_element_by_css_selector(".icon.calendarDay.ng-scope").click()
+                    # Continue with current guest list
+                    driver.find_element_by_xpath("""//*[@id="selectPartyPage"]/div[3]/div/div[2]/div""").click()
+    
+                    break
+
+        
+        # if yes, get earliest time with atleast 1 hour inbetween fast passes.
+
+        # What happens when the desired fast pass isn't available at all?
+
+        # fast pass not available -----  <span ng-if="experience.offersets.length === 0" class="icon fpCancel noAvailableTimes ng-scope"></span>
+
+
 
 
